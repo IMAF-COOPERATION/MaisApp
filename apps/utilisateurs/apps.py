025948031f0be76bfcab13e00777aa8f707b79a4
IMAF-COOPERATION <imafcooperation@gmail.com>
2025-08-04 14:02:23 +0200

    Update apps.py

--- apps/utilisateurs/apps.py
+++ apps/utilisateurs/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class UtilisateursConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.utilisateurs'  # ← bien mettre ça
+    name = 'apps.utilisateurs'
